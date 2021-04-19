986425fe1e3934026d83235ad8ea13fb1e703937
brieannabetts <bgbetts@lakeheadu.ca>
2021-04-19 14:30:41 -0500

    third 30 min commit

--- comp1006g-final-21-master/profile.php
+++ comp1006g-final-21-master/profile.php
@@ -3,9 +3,39 @@ $pageTitle = 'My Profile';
 include 'header.php';
 include 'auth.php';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Profile</title>
+</head>
+<body>
 <h1>My Profile</h1>
+<form method="post" action="save-profile.php">
+    <fieldset>
+    </fieldset>
+    <button>Save</button>
+</form>
+</body>
+</html>
 <?php
 //connect
 include 'db.php';
+$sql ="SELECT username FROM examusers";
+$cmd = $db->prepare($sql);
+$cmd->execute();
+$username = $cmd->fetchAll();
+if (!$username){
+    echo '<div class="alert alert-danger">No User found</div>';
+}
+else{
+    echo '<table class="table table-striped table-light sortable">
+    <thead>
+                <th><a>User</a></th>';
+
+}
+$db =null;
 ?>
+<?php include 'footer.php'; ?>
+
 
--- comp1006g-final-21-master/save-profile.php
+++ comp1006g-final-21-master/save-profile.php
@@ -1,1 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Saving your Profile</title>
+</head>
+<body>
 <?php
+// 1. store the form inputs in variables (optional but reduces syntax errors)
+$photo = $_POST['photo'];
+$ok = true;
+
+if ($ok) {
+    // 2. connect to db
+    include 'db.php';
+
+    // 3. set up an SQL INSERT command w/2 parameters that have : prefixes
+    $sql = "INSERT INTO examusers";
+
+    // 4. populate the INSERT with our variables using a Command variable to prevent SQL Injection
+    $cmd = $db->prepare($sql);
+    $cmd->bindParam(':name', $photo, PDO::PARAM_STR, 50);
+
+    // 5. execute the INSERT to save the data
+    $cmd->execute();
+
+    // 6. disconnect
+    $db = null;
+
+    // 7. show confirmation message to user
+    echo "<h1>Profile Saved</h1>";
+    header('location:profile.php');
+}
+
+?>
+</body>
+</html>
