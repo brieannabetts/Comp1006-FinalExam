627befa1d205fae47c02ed833810b02e89243332
brieannabetts <bgbetts@lakeheadu.ca>
2021-04-19 13:13:59 -0500

    db updated

--- comp1006g-final-21-master/db.php
+++ comp1006g-final-21-master/db.php
@@ -1,6 +1,6 @@
 <?php
 // aws
-$db = new PDO('mysql:host=172.31.22.43;dbname=', '', '');
+$db = new PDO('mysql:host=172.31.22.43;dbname=Brieanna1158732', 'Brieanna1158732', 'z0PV1TNW9O');
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
